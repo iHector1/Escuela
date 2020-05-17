9c00f8e6c0718a481b644193ed357901493931dd
iHector1 <62352013+iHector1@users.noreply.github.com>
2020-05-17 18:49:52 -0500

    a

--- Controllers/ProfesorController.cs
+++ Controllers/ProfesorController.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Escuela.Model;
+using Escuela.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Escuela.Models;
-using Escuela.Model;
-namespace Escuela.Controllers
-{
-    public class ProfesorController : Controller
-    {
-        public IActionResult lista_profesores()
-        {
-            EscuelaFULLContext db = new EscuelaFULLContext();
-            return View(db.Profesor.ToList());
+namespace Escuela.Controllers {
+    public class ProfesorController : Controller {
+        public IActionResult lista_profesores () {
+            EscuelaFULLContext db = new EscuelaFULLContext ();
+            return View (db.Profesor.ToList ());
         }
 
+        public IActionResult Crear_Profesores () {
+
+            return View ();
+        }
+        [HttpPost]
+        public IActionResult Crear_Profesores (Profesor profesor) {
+
+            return View ();
+        }
     }
 }
